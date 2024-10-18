f33060ebc64ca10a0dbca7d9fe715968a84a9743
AtirhGael <atirhg@gmail.com>
2024-10-18 22:02:58 +0100

    added fonts

--- App.tsx
+++ App.tsx
@@ -4,8 +4,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigator/RootNavigator';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store/store';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Jakarta-Bold": require("./src/assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("./src/assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("./src/assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("./src/assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("./src/assets/fonts/PlusJakartaSans-Medium.ttf"),
+    Jakarta: require("./src/assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("./src/assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
   return (
     <Provider store={store}>
       <NavigationContainer>
